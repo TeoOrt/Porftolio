81566d0771d6bd506af70076a8d72baf67b9b1f1
Mateo Ortega <mateo041ortega@gmail.com>
2023-05-07 23:56:07 -0600

    Porfolio Push

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const sticky_navbar = () => {
+    let navbar = document.querySelector(".NavBar");
+    let sticky = navbar.offsetTop;
+    
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  };
+  
+//   window.addEventListener("load", sticky_navbar);
+  window.addEventListener("scroll", sticky_navbar);
+  
