f6d90806153ed4c43888a65a2fd1585be541b50c
TeoOrt <mateo041ortega@gmail.com>
2024-03-03 00:34:49 -0700

    Reworked css

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
+
+  const goToIds = ['AboutClick','SkillsClicked','ProjectClicked','ContactsClicked']
+
+  goToIds.map((ids) => {
+    document.getElementById(ids).addEventListener('click', ()=>{
+      const mobileMenu = document.getElementById('mobile-menu');
+      mobileMenu.classList.toggle('hidden');    
+    });  
+  })
+
+  
+
   document.getElementById('mobile-menu-toggle').addEventListener('click', function() {
     var mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.classList.toggle('hidden');
