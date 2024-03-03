3cc32bdc28e6ad1ac75bd22c5817d7775eeb856a
TeoOrt <mateo041ortega@gmail.com>
2024-03-02 20:34:37 -0700

    Reworked css

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
-const sticky_navbar = () => {
-  let navbar = document.querySelector(".NavBar");
-  let sticky = navbar.offsetTop;
 
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-};
 
-window.addEventListener("load", sticky_navbar);
-window.addEventListener("scroll", sticky_navbar);
+document.addEventListener('DOMContentLoaded', function() {
+
+  document.getElementById('mobile-menu-toggle').addEventListener('click', function() {
+    var mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+  })});
